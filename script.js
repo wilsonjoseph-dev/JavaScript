364fa6c1994dbb1fa00eafb34bc6ece41876c4db
wilson-joseph <141981812+wilsonjoseph-dev@users.noreply.github.com>
2024-04-27 19:10:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+/* const body = document.querySelector("body");
+body.innerHTML = ("<div class=container></div>");
+
+const div = document.querySelector(".container");
+div.innerHTML = ("<h1 class=para1></h1>");
+
+const para1 = document.querySelector(".para1");
+para1.textContent = ("Hello World");
+style.para1 = "font-color: blue;" */
+
+
